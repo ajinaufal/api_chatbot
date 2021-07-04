52da37a29e6d0bf299175d953345ee81291623d6
ajinaufal <ajinaufal69@gmail.com>
2021-07-04 16:26:01 +0700

    deploy

--- train_chatbot.py
+++ train_chatbot.py
@@ -12,6 +12,7 @@ from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD
 from keras.optimizers import Adam
 from keras.optimizers import Adagrad
+from keras.optimizers import Adamax
 import random
 
 from keras.utils.vis_utils import plot_model
@@ -78,6 +79,7 @@ print("Training data created")
 
 # Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
 # equal to number of intents to predict output intent with softmax
+
 model = Sequential()
 model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.1))
@@ -89,9 +91,10 @@ plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=T
 
 print(model.summary())
 # Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+adx = Adamax(learning_rate=0.02)
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer=adx, metrics=['accuracy'])
 
 #fitting and saving the model
 hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
